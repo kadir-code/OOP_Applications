3257c28a455bf58ef397088eb6d6428d301faea3
kadir <56607309+kadir-code@users.noreply.github.com>
2021-10-11 16:10:42 +0300

    Add files via upload

---
+++ Custom_Exception/Custom_Exception/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Custom_Exception
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SendMail sendMail = new SendMail();
+        public void CheckText(string text)//kullanıcıdan geleck parametre
+        {
+            foreach (char item in text)//döngü ile gelen parametreyi karakter karakter dolaşıyoruz sayı var mı diye
+            {
+                if (char.IsDigit(item))//gelen değerlerden biri digit ise if devrreye girecek
+                {
+                    throw new HasNumber(text);//custome hatamızı fırlatacağız
+                }
+            }
+        }
+        public void WriteListVeriGirisi()
+        {
+            listView1.Items.Clear();
+            StreamReader streamReader = new StreamReader(Application.StartupPath + @"\log.txt");
+            string[] exceptions = streamReader.ReadToEnd().Split(new string[] { "\r\n" },
+                StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string item in exceptions)
+            {
+                string[] degerler = item.Split('|');
+                ListViewItem listViewItem = new ListViewItem();
+                listViewItem.Text = degerler[0];
+                listViewItem.SubItems.Add(degerler[1]);
+                listViewItem.SubItems.Add(degerler[2]);
+
+                listView1.Items.Add(listViewItem);
+            }
+            listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+            streamReader.Close();
+
+        }
+        private void BtnVeriGirisi_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                CheckText(txtVeriGirisi.Text);
+            }
+            catch (Exception ex)
+            {
+                errorProvider1.SetError(txtVeriGirisi,ex.Message);
+                sendMail.SendErrorMail();
+                WriteListVeriGirisi();
+            }
+        }
+    }
+}
---
+++ Custom_Exception/Custom_Exception/SendMail.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Custom_Exception
+{
+    public class SendMail
+    {
+         HasNumber hasNumber = new HasNumber();
+
+        public string SendErrorMail()
+        {
+            MailMessage mail = new MailMessage();
+            mail.To.Add("Your e-mail here");
+            mail.From = new MailAddress("Your e-mail here");
+            mail.Subject = "You Have a Mail";
+            mail.Body = "Dear user, you have a mail from" + "Jane.Doe" + ".<br/>" + hasNumber.Message;
+            mail.IsBodyHtml = true;
+
+            SmtpClient smtp = new SmtpClient();
+            smtp.Credentials = new NetworkCredential("Your e-mail here", "Your e-mail password here*");
+            smtp.Port = 587;
+            smtp.Host = "smtp.gmail.com";
+            smtp.EnableSsl = true;
+
+            try
+            {
+                smtp.Send(mail);
+               return "Your mail send successfuly";
+
+            }
+            catch (Exception ex)
+            {
+                return $"Can not send mail because ; {ex.Message}";
+            }
+
+        }
+    }
+}
